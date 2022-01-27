3ff39fc653ae7849ddd92c1e3012f77ff00c5277
Ashesh Anand <62696185+ashesh705@users.noreply.github.com>
2022-01-27 15:24:39 +0530

    Repo Setup

---
+++ src/dummy.py
@@ -0,0 +1,9 @@
+""" Dummy source code to initialize repo"""
+
+from typing import Literal
+
+
+def dummy() -> Literal[True]:
+    """Dummy function"""
+
+    return True
---
+++ tests/test_dummy.py
@@ -0,0 +1,9 @@
+""" Dummy test case to initialize repo"""
+
+from src.dummy import dummy
+
+
+def test_dummy() -> None:
+    """Dummy test case"""
+
+    assert dummy() is True
